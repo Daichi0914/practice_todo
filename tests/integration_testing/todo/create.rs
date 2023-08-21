40eba64dfe0de08b779aad4c3401db73c3042650
Daichi44 <DaichiSugawara44@gmail.com>
2023-08-21 19:36:57 +0900

    Red: Added create todo test

---
+++ tests/integration_testing/todo/create.rs
@@ -0,0 +1,16 @@
+use axum::http::StatusCode;
+use crate::integration_testing::test_helper::response::body_to_string;
+use crate::integration_testing::test_helper::response::get_method::call_api_with_body;
+
+#[tokio::test]
+async fn todoを作成する() {
+    // given
+    let uri = format!("/api/v1/todo");
+    let body = format!(r#"{{ "todo": "運動する" }}"#);
+
+    // when
+    let response = call_api_with_body("POST", &uri, body).await;
+
+    assert_eq!(response.status(), StatusCode::NO_CONTENT);
+    assert_eq!(body_to_string(response).await, "");
+}
