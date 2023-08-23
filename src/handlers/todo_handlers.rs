3e9d39f04c72bd71917c21cb0ec0685ee8023f3a
Daichi44 <DaichiSugawara44@gmail.com>
2023-08-23 21:57:22 +0900

    Added Todo struct

--- src/handlers/todo_handlers.rs
+++ src/handlers/todo_handlers.rs
@@ -1,27 +1,46 @@
-fn create_todo(todo: &str) -> &str {
-    todo
-}
+use serde::Serialize;
 
-#[derive()]
+#[derive(PartialEq, Serialize, Debug, Clone)]
 enum TodoStatus {
     Done,
     InProgress,
     Undone
 }
 
-struct Todo {
+#[derive(PartialEq, Serialize, Debug, Clone)]
+pub struct Todo {
     id: i32,
     action: String,
     status: TodoStatus,
+}
+
+impl Todo {
+    fn new(action: String, status: TodoStatus) -> Self {
+        Todo {
+            id: 1,
+            action,
+            status
+        }
+    }
+
+    fn create_todo(todo: Todo) -> Todo {
+        todo
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn todoを返す() {
+    #[tokio::test]
+    async fn todoを返す() {
+        // given
+        let todo = Todo::new(String::from("掃除をする"), TodoStatus::Undone);
+
+        // when
+        let result = Todo::create_todo(todo.clone());
+
         // then
-        assert_eq!(create_todo("掃除をする"), "掃除をする")
+        assert_eq!(result, todo)
     }
 }
