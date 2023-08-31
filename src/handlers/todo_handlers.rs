3c9c006087ed5063018adc061d46a26b6570d018
Daichi44 <DaichiSugawara44@gmail.com>
2023-08-31 20:35:21 +0900

    Green: Modified type

--- src/handlers/todo_handlers.rs
+++ src/handlers/todo_handlers.rs
@@ -1,7 +1,9 @@
+use std::sync::Arc;
+use axum::Extension;
 use crate::models::todo_models::{CreateTodo, Todo, TodoRepository, TodoRepositoryForMemory};
 
-fn create_todo(payload: CreateTodo, memory: TodoRepositoryForMemory) -> Todo {
-    let todo = memory.create(payload);
+fn create_todo(payload: CreateTodo, repository: Extension<Arc<TodoRepositoryForMemory>>) -> Todo {
+    let todo = repository.create(payload);
     todo
 }
 
@@ -10,7 +12,7 @@ mod tests {
     use std::sync::Arc;
     use axum::Extension;
     use crate::handlers::todo_handlers::create_todo;
-    use crate::models::todo_models::{CreateTodo, TodoRepository, TodoRepositoryForMemory};
+    use crate::models::todo_models::{CreateTodo, Todo, TodoRepository, TodoRepositoryForMemory};
 
     #[test]
     fn todoを作成した後そのtodoが返ってくる() {
@@ -23,6 +25,9 @@ mod tests {
         let result = create_todo(payload, Extension(arc_repository));
 
         // then
-        assert_eq!(result.get("action"), payload.action)
+        assert_eq!(
+            result,
+            Todo::new(1, String::from("帰宅する"))
+        )
     }
 }
